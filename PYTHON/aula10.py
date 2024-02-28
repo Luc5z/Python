00bad3b6bffe74033ac0552f28f927c07185eb01
Dival Lucas <lusca404@gmail.com>
2024-02-28 17:11:48 -0300

    teste

---
+++ PYTHON/aula10.py
@@ -0,0 +1,18 @@
+# Só aceitar produto se o valor for entre 20 e 40 (verificar se um valor está entre 2 valores)
+
+valor = 20
+
+
+if valor >= 20 and valor < 40: # --> Jeito paia <--
+    print('Produto aceito')
+else:
+    print('Produto negado')
+
+
+if 20 <= valor < 40: # --> Jeito mais organizado e limpo
+    print('Produto aceito')
+else:
+    print('Produto negado')
+
+
+# py aula10.py
