930053bfbfd9b1b9aace4fc40e489f89aaa52cfc
Dival Lucas <lusca404@gmail.com>
2024-03-27 11:29:10 -0300

    Day 11

--- PYTHON/aula26.py
+++ PYTHON/aula26.py
@@ -11,7 +11,7 @@ if verifica_numero:
     if int(numero_str) % 2 == 0:
         print(f'O número {numero_str} é par')
     else:
-        print(f'O número {numero_str} não é par')
+        print(f'O número {numero_str} é ímpar')
 else:
     print(f'{numero_str} não é um inteiro') 
 
@@ -21,8 +21,28 @@ descrito, exiba a saudação apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
+horaUsuario = int(input('Que horas são? '))
+
+if 0 <= horaUsuario <= 11:
+    print('Bom dia!')
+elif 12 <= horaUsuario <= 17:
+    print('Boa tarde!')
+elif 18 <= horaUsuario <= 23:
+    print('Boa noite!')
+else:
+    print('Erro')
+
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+
+nome = input('Qual seu primeiro nome? ')
+
+if len(nome) <= 4:
+    print('Seu nome é muito curto')
+elif 5 <= len(nome) <= 6:
+    print('Seu nome é normal')
+else:
+    print('Seu nome é muito grande')
