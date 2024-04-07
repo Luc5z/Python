725e5112477e1ab18fa8a5e392545c9608f78781
Dival Lucas <lusca404@gmail.com>
2024-04-07 18:07:11 -0300

    day 15

--- PYTHON/aula31.py
+++ PYTHON/aula31.py
@@ -1,6 +1,23 @@
-# introdução á type list
-#        0    1      2        3
-lista = [123, True, 'string', []]
-
-print(lista)
-print()
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(50) # adiciona valor ao final
+lista.pop() # tira o valor do final
+lista.append(60)
+lista.append(70)
+ultimo_valor = lista.pop(3) # o 'pop' retorna o valor.
+#                   'pop(3)' pega o indice 3 e tira da lista
+print(lista, 'Removido,', ultimo_valor)
---
+++ PYTHON/aula32.py
@@ -0,0 +1,14 @@
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+indice = 0
+
+for nome in lista:
+    print(f'{nome} está no indice {indice}')
+    indice += 1
+print()
