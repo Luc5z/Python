6c1de658c5cde7beca3229b2464f82ab88ed76cc
Dival Lucas <lusca404@gmail.com>
2024-04-07 18:13:37 -0300

    Introducing of packing and unpacking

---
+++ PYTHON/aula33.py
@@ -0,0 +1,9 @@
+"""
+Introdução ao empacotamento e desempacotamento
+"""
+# * faz com o que tudo que sobrar da lista va para variavel 'resto'
+
+_, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
+
+# _ é o nome que damos para uma variavel que nao vamos usar
+print(nome)
