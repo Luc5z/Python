9601628e55d482d4b7fbc8970551326489d7b21f
Dival Lucas <lusca404@gmail.com>
2024-04-07 22:50:49 -0300

    A working to buy list

---
+++ PYTHON/aula35.py
@@ -0,0 +1,59 @@
+"""
+Faça uma lista de compras com listas
+O usuário deve ter a possibilidade de
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com 
+erros de índices inexistentes na lista.
+"""
+
+import os
+
+actions = 'ials'
+toBuyList = []
+
+while True:
+    action = input('Selecione uma opção: \n[i]nserir [a]pagar [l]istar [s]air \n')
+
+    if len(action) > 1 or action not in actions:
+        print('Por favor digite uma opção válida.')
+        print()
+        continue
+    
+    if action == 'i':
+        os.system('cls')
+        valor = input('Valor: ')
+        toBuyList.append(valor)
+        print()
+
+    elif action == 'a':
+        os.system('cls')
+        for index, item in enumerate(toBuyList):
+            print(index, item)
+        print()
+
+        valor = input('O que você deseja apagar? ')
+        try:
+            toBuyList.pop(int(valor))
+        except:
+            print('Insira um índice válido.')
+            print()
+            continue
+
+    elif action == 'l':
+        os.system('cls')
+        if toBuyList:
+            for index, item in enumerate(toBuyList):
+                print(index, item)
+            print()
+        else:
+            print('Lista vazia.')
+            print()
+            continue
+
+    elif action == 's':
+        os.system('cls')
+        print('Saindo...')
+        print()
+        break
+
+
