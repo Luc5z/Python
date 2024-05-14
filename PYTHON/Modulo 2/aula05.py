dfca1ac1b77e772dc7e021b1a7d00dd2860da005
Dival Lucas <lusca404@gmail.com>
2024-05-14 19:15:24 -0300

    introducing closure

--- PYTHON/Modulo 2/aula05.py
+++ PYTHON/Modulo 2/aula05.py
@@ -1,4 +1,13 @@
-def criar_saudacao(saudacao, nome):
-    return f'{saudacao}, {nome}!'
+# Closure 
 
-s1 = 
+def criar_saudacao(saudacao, nome):
+    def saudar():
+
+        return f'{saudacao}, {nome}!'
+    return saudar # retorna a função estruturada mas não executada
+
+s1 = criar_saudacao('Bom dia', 'luiz') 
+s2 = criar_saudacao('Bom dia', 'lucas') 
+
+print(s1()) # executa a função
+print(s2())
