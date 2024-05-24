fbd0431fa677d190fb6da036f018a75546513728
Dival Lucas <lusca404@gmail.com>
2024-05-24 20:28:59 -0300

    dictionary methods

---
+++ PYTHON/Modulo 2/aula08.py
@@ -0,0 +1,23 @@
+pessoa = {
+    'nome': 'Luiz Otávio',
+    'sobrenome': 'Miranda',
+}
+# valor padrão caso nao exista a chave ou não possua valor:
+pessoa.setdefault('idade', none)
+
+# como trabalhar com dicionarios:
+print(list(pessoa.items()))
+print(list(pessoa.keys()))
+print(list(pessoa.values()))
+
+# Métodos úteis dos dicionários em Python
+# len - quantas chaves
+# keys - iterável com as chaves
+# values - iterável com os valores
+# items - iterável com chaves e valores
+# setdefault - adiciona valor se a chave não existe
+# copy - retorna uma cópia rasa (shallow copy)
+# get - obtém uma chave
+# pop - Apaga um item com a chave especificada (del)
+# popitem - Apaga o último item adicionado
+# update - Atualiza um dicionário com outro
