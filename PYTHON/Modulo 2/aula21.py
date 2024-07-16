1bdfdab399206fcc818eefa77677e184b3a65ba7
Dival Lucas <lusca404@gmail.com>
2024-07-16 10:31:02 -0300

    zipper exercise (joining lists)

---
+++ PYTHON/Modulo 2/aula21.py
@@ -0,0 +1,21 @@
+# Exercício - Unir listas
+# Crie uma função zipper (como o zipper de roupas)
+# O trabalho dessa função será unir duas
+# listas na ordem.
+# Use todos os valores da menor lista.
+# Ex.:
+# ['Salvador', 'Ubatuba', 'Belo Horizonte']
+# ['BA', 'SP', 'MG', 'RJ']
+# Resultado
+# [('Salvador', 'BA'), ('Ubatuba', 'SP'), ('Belo Horizonte', 'MG')]
+
+l1 = ['Salvador', 'Ubatuba', 'Belo Horizonte']
+l2 = ['BA', 'SP', 'MG', 'RJ']
+
+def zipper(lista1, lista2):
+    intervalo_maximo = min(len(lista1), len(lista2))
+    return[
+        (lista1[i], lista2[i]) for i in range(intervalo_maximo)
+    ]
+
+print(zipper(l1, l2))
